86c094ce3cf173bc7258d0ebdd364ef3e39b17ca
jwhite <jtwhite79@yahoo.com>
2016-03-03 09:42:44 -0600

    polishing errvar and nsmc notebooks.  renamed "drop" freyberg components

--- examples/Freyberg_ss/start_slaves.py
+++
@@ -1,9 +0,0 @@
-import os
-import pyemu
-
-exe = os.path.join("exe","pest++.exe")
-slave_dir = "."
-pst = "freyberg.pst"
-num_slaves = 15
-
-pyemu.pst_utils.start_slaves(slave_dir,exe,pst,num_slaves=num_slaves)
