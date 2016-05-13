09613dfa4bb1ad0d4218ae910e27714e1204cb2f
jwhite <jtwhite79@yahoo.com>
2016-05-13 11:20:47 -0500

    added more geostats tests

--- pyemu/tests/utils_tests.py
+++ pyemu/tests/utils_tests.py
@@ -19,7 +19,32 @@ def vario_test():
         assert v.h_function(0.0) == contribution
         assert v.h_function(a*1000) == 0.0
 
+        v2 = const(contribution,a,anisotropy=2.0,angle=90.0)
+        print(v2.h_function(a))
 
+
+def aniso_test():
+
+    import pyemu
+    contribution = 0.1
+    a = 2.0
+
+
+
+    for const in [pyemu.utils.geostats.ExpVario,pyemu.utils.geostats.GauVario,
+                  pyemu.utils.geostats.SphVario]:
+
+        v = const(contribution,a)
+        v2 = const(contribution,a,anisotropy=2.0,angle=90.0)
+        v3 = const(contribution,a,anisotropy=2.0,angle=0.0)
+        pt0 = (0,0)
+        pt1 = (1,0)
+        assert v.covariance(pt0,pt1) == v2.covariance(pt0,pt1)
+
+        pt0 = (0,0)
+        pt1 = (0,1)
+        assert v.covariance(pt0,pt1) == v3.covariance(pt0,pt1)
+        
 
 def geostruct_test():
     import pyemu
@@ -30,10 +55,17 @@ def geostruct_test():
     g = pyemu.utils.geostats.GeoStruct(0.2,[v1,v2,v3])
     pt0 = (0,0)
     pt1 = (0,0)
-    print(g.covariance(pt0,pt1))
+    assert g.covariance(pt0,pt1) == 0.5
+
+    pt0 = (0,0)
+    pt1 = (1.0e+10,0)
+    assert g.covariance(pt0,pt1) == 0.2
+
+
 
 
 if __name__ == "__main__":
     #fac2real_test()
     #vario_test()
-    geostruct_test()
+    #geostruct_test()
+    aniso_test()
