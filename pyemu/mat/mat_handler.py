bug fix in Cov.from_parameter_data: use abs value in log-based range calculation, added parameter perturbation calculations to Pst, fixed a few issues in the schur freyberg notebook

pyemu/mat/mat_handler.py
@@ -1552,7 +1552,7 @@ class Cov(Matrix):
             ub = row["parubnd"] * row["scale"] + row["offset"]
 
             if t == "log":
-                var = ((np.log10(ub) - np.log10(lb)) / 4.0) ** 2
+                var = ((np.log10(np.abs(ub)) - np.log10(np.abs(lb))) / 4.0) ** 2
             else:
                 var = ((ub - lb) / 4.0) ** 2
             if np.isnan(var):

pyemu/pst/pst_handler.py
@@ -192,7 +192,7 @@ class Pst(object):
         """observation groups
         """
         og = list(self.observation_data.groupby("obgnme").groups.keys())
-        #og = map(pst_utils.SFMT, og)
+        #og = list(map(pst_utils.SFMT, og))
         return og
 
 
@@ -209,7 +209,7 @@ class Pst(object):
         """prior info groups
         """
         og = list(self.prior_information.groupby("obgnme").groups.keys())
-        og = map(pst_utils.SFMT, og)
+        #og = list(map(pst_utils.SFMT, og))
         return og
 
     @property
@@ -556,8 +556,8 @@ class Pst(object):
                                             index=False)+'\n')
             #self.tied.loc[:,"parnme"] = self.tied.index
         f_out.write("* observation groups\n")
-        [f_out.write(str(group)+'\n') for group in self.obs_groups]
-        [f_out.write(str(group)+'\n') for group in self.prior_groups]
+        [f_out.write(pst_utils.SFMT(str(group))+'\n') for group in self.obs_groups]
+        [f_out.write(pst_utils.SFMT(str(group))+'\n') for group in self.prior_groups]
 
         f_out.write("* observation data\n")
         #self.observation_data.index = self.observation_data.pop("obsnme")
@@ -923,3 +923,90 @@ class Pst(object):
                 ow = min(wmax, nw)
             new_weights.append(ow)
         self.observation_data.weight = new_weights
+
+    def calculate_pertubations(self):
+
+        self.build_increments()
+        self.parameter_data.loc[:,"pertubation"] = \
+            self.parameter_data.parval1 + \
+            self.parameter_data.increment
+
+        self.parameter_data.loc[:,"out_forward"] = \
+            self.parameter_data.loc[:,"pertubation"] > \
+            self.parameter_data.loc[:,"parubnd"]
+
+        out_forward = self.parameter_data.groupby("out_forward").groups
+        if True in out_forward:
+            self.parameter_data.loc[out_forward[True],"pertubation"] = \
+                    self.parameter_data.loc[out_forward[True],"parval1"] - \
+                    self.parameter_data.loc[out_forward[True],"increment"]
+
+            self.parameter_data.loc[:,"out_back"] = \
+            self.parameter_data.loc[:,"pertubation"] < \
+            self.parameter_data.loc[:,"parlbnd"]
+            out_back = self.parameter_data.groupby("out_back").groups
+            if True in out_back:
+                still_out = out_back[True]
+                print(self.parameter_data.loc[still_out,:],flush=True)
+
+                raise Exception("Pst.calculate_pertubations(): " +\
+                                "can't calc pertubations for the following "+\
+                                "parameters: {0}".format(','.join(still_out)))
+
+    def build_increments(self):
+        self.enforce_bounds()
+
+        par_groups = self.parameter_data.groupby("pargp").groups
+        inctype = self.parameter_groups.groupby("inctyp").groups
+        for itype,inc_groups in inctype.items():
+            pnames = []
+            for group in inc_groups:
+                pnames.extend(par_groups[group])
+                derinc = self.parameter_groups.loc[group,"derinc"]
+                self.parameter_data.loc[par_groups[group],"derinc"] = derinc
+            if itype == "absolute":
+                self.parameter_data.loc[pnames,"increment"] = \
+                    self.parameter_data.loc[pnames,"derinc"]
+            elif itype == "relative":
+                self.parameter_data.loc[pnames,"increment"] = \
+                    self.parameter_data.loc[pnames,"derinc"] * \
+                    self.parameter_data.loc[pnames,"parval1"]
+            elif itype == "rel_to_max":
+                mx = self.parameter_data.loc[pnames,"parval1"].max()
+                self.parameter_data.loc[pnames,"increment"] = \
+                    self.parameter_data.loc[pnames,"derinc"] * mx
+            else:
+                raise Exception('Pst.get_derivative_increments(): '+\
+                                'unrecognized increment type:{0}'.format(itype))
+
+        #account for fixed pars
+        isfixed = self.parameter_data.partrans=="fixed"
+        self.parameter_data.loc[isfixed,"increment"] = \
+            self.parameter_data.loc[isfixed,"parval1"]
+
+    def add_transform_columns(self):
+        for col in ["parval1","parlbnd","parubnd","increment"]:
+            if col not in self.parameter_data.columns:
+                continue
+            self.parameter_data.loc[:,col+"_trans"] = (self.parameter_data.parval1 *
+                                                          self.parameter_data.scale) +\
+                                                         self.parameter_data.offset
+            isfixed = self.parameter_data.partrans == "fixed"
+            self.parameter_data.loc[isfixed,col+"_trans"] = \
+                self.parameter_data.loc[isfixed,col+"_trans"].\
+                    apply(lambda x:np.log10(x))
+
+    def enforce_bounds(self):
+        too_big = self.parameter_data.loc[:,"parval1"] > \
+            self.parameter_data.loc[:,"parubnd"]
+        self.parameter_data.loc[too_big,"parval1"] = \
+            self.parameter_data.loc[too_big,"parubnd"]
+
+        too_small = self.parameter_data.loc[:,"parval1"] < \
+            self.parameter_data.loc[:,"parlbnd"]
+        self.parameter_data.loc[too_small,"parval1"] = \
+            self.parameter_data.loc[too_small,"parlbnd"]
+
+
+
+

pyemu/utils/optimization.py
@@ -8,9 +8,33 @@ OPERATOR_WORDS = ["l","g","n","e"]
 OPERATOR_SYMBOLS = ["<=",">=","=","="]
 
 
-def to_mps(jco,obj_func=None,obs_constraint_sense=None,pst=None,decision_var_names=None,
-           mps_filename=None,):
+def to_mps(jco,obj_func=None,obs_constraint_sense=None,pst=None,
+           decision_var_names=None,mps_filename=None,):
+    """helper utility to write an mps file from pest-style
+    jacobian matrix. Requires corresponding pest control
+    file.
 
+    Parameters:
+        jco : pyemu.Matrix or str (filename of matrix)
+        obj_func : optional.  If None, an obs group must exist
+            named 'n' and must have one one member.  Can be a str, which
+            is the name of an observation to treat as the objective function
+            or can be a dict, which is keyed on decision var names and valued
+            with objective function coeffs.
+        obs_constraint_sense : optional.  If None, obs groups are sought that
+            have names "l","g", or "e" - members of these groups are treated
+            as constraints.  Otherwise, must be a dict keyed on constraint
+             (obs) names with values of "l","g", or "e".
+        pst : optional.  If None, a pest control file is sought with
+            filename <case>.pst.  Otherwise, must be a pyemu.Pst instance or
+            a filename of a pest control file
+        decision_var_names: optional.  If None, all parameters are treated as
+            decision vars. Otherwise, must be a list of str of parameter names
+            to use as decision vars
+        mps_filename : optional.  If None, then <case>.mps is written.
+            Otherwise, must be a str.
+
+    """
     if isinstance(jco,str):
         pst_name = jco.lower().replace('.jcb',".pst").replace(".jco",".pst")
         jco = Matrix.from_binary(jco)
@@ -148,6 +172,26 @@ def to_mps(jco,obj_func=None,obs_constraint_sense=None,pst=None,decision_var_nam
     if mps_filename is None:
         mps_filename = pst.filename.replace(".pst",".mps")
 
+    resp_mat = jco.get(row_names=order_obs_constraints,col_names=order_dec_var)
+
+    #curr_dec_vec = Matrix.from_dataframe(
+    #        pst.parameter_data.loc[order_dec_var,["parval1"]])
+
+    pst.calculate_pertubations()
+    curr_dec_vec = Matrix.from_dataframe(
+            pst.parameter_data.loc[order_dec_var,["pertubation"]])
+
+    rhs = np.atleast_2d(
+            np.array(([rhs[name] for name in order_obs_constraints]))).transpose()
+
+    curr_rhs = Matrix(
+            x=rhs,row_names=order_obs_constraints,col_names=["pertubation"])
+
+    mod_rhs = (resp_mat * curr_dec_vec)
+
+    dist_rhs = mod_rhs - curr_rhs
+
+
     with open(mps_filename,'w') as f:
         f.write("NAME {0}\n".format("pest_opt"))
         f.write("ROWS\n")
@@ -167,12 +211,12 @@ def to_mps(jco,obj_func=None,obs_constraint_sense=None,pst=None,decision_var_nam
                                jco.x[jco_iidx,jco_jidx]))
             f.write("    {0:8}  {1:8}   {2:10G}\n".\
                     format(new_decision_names[dname],
-                           obj_name,obj[dname]))
+                           obj_name,pst.parameter_data.loc[dname,"pertubation"]))
 
         f.write("RHS\n")
-        for name in order_obs_constraints:
+        for iname,name in enumerate(order_obs_constraints):
             f.write("    {0:8}  {1:8}   {2:10G}\n".
-                    format("rhs",new_constraint_names[name],rhs[name]))
+                    format("rhs",new_constraint_names[name],dist_rhs.x[iname,0]))
         f.write("BOUNDS\n")
         for name in order_dec_var:
             up,lw = pst.parameter_data.loc[name,"parubnd"],\