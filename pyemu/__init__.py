3cf44aade3bda68fe63c53fe1b4676f83061cf65
Jeremy White <jtwhite1000@gmail.com>
2018-03-02 14:22:43 +1300

    removed influence module - who uses those stats anyway?  They are really meaningless in real-world scale problems

--- pyemu/__init__.py
+++ pyemu/__init__.py
@@ -13,7 +13,7 @@ from .sc import Schur
 from .ev import ErrVar
 from .en import Ensemble, ParameterEnsemble, ObservationEnsemble
 from .mc import MonteCarlo
-from .inf import Influence
+#from .inf import Influence
 from .smoother import EnsembleSmoother
 from .mat import Matrix, Jco, Cov, concat
 from .pst import Pst, pst_utils
