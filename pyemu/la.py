3857863bd1b49b3764727fa3e9aa391cd826b65f
Mike Fienen <mnfienen@usgs.gov>
2018-08-09 15:46:53 -0500

    reveerting back to undo a change that needs more attention
    Revert "another tiny docstring change"
    
    This reverts commit 4572304eb3c551f5e2f562555b8c67f53ea095ec.

--- pyemu/la.py
+++ pyemu/la.py
@@ -41,11 +41,11 @@ class LinearAnalysis(object):
         the file extension.  If None, the observation noise covariance matrix is
         constructed from the weights in the control file represented
         by the pst argument. Can also be a pyemu.Cov instance
-    forecasts or predictions : (varies)
+    forecasts : (varies)
         prediction (aka forecast) sensitivity vectors.  If str, a filename
         is assumed and predictions are loaded from a file using the file extension.
         Can also be a pyemu.Matrix instance, a numpy.ndarray or a collection
-        of pyemu.Matrix or numpy.ndarray. Note this is allowing either term to be used
+        of pyemu.Matrix or numpy.ndarray.
     ref_var : (float)
         reference variance
     verbose : (either bool or string)
