945c8574178da497aeca401563f6f5426849367d
Jeremy White <jtwhite1000@gmail.com>
2018-08-30 09:52:08 +1200

    added skeleton evol_proto.py

---
+++ pyemu/evol_proto.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+import pandas as pd
+
+import pyemu
+from pyemu.smoother import EnsembleMethod
+
+
+def EvolAlg(EnsembleMethod):
+    def __init__(self, pst, parcov = None, obscov = None, num_slaves = 0, use_approx_prior = True,
+                 submit_file = None, verbose = False, port = 4004, slave_dir = "template"):
+        super(EvolAlg, self).__init__(pst=pst, parcov=parcov, obscov=obscov, num_slaves=num_slaves,
+                                      submit_file=submit_file, verbose=verbose, port=port,
+                                      slave_dir=slave_dir)
+
+    def initialize(self):
+        pass
+
+    def update(self):
+        pass
+
+    
+
+
+
+
+
+
+
+
+
