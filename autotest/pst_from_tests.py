1e15ffb371a18baef5e9026a4df642720f78aa8b
Jeremy White <jtwhite1000@gmail.com>
2018-10-08 12:48:36 +1300

    starting to outline new PstFrom

---
+++ autotest/pst_from_tests.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import platform
+
+#sys.path.append(os.path.join("..","pyemu"))
+from pyemu import os_utils
+from pyemu.prototypes.pst_from import PstFrom
+
+
+ext = ''
+bin_path = os.path.join("..", "..", "bin")
+if "linux" in platform.platform().lower():
+    bin_path = os.path.join(bin_path, "linux")
+elif "darwin" in platform.platform().lower():
+    bin_path = os.path.join(bin_path, "mac")
+else:
+    bin_path = os.path.join(bin_path, "win")
+    ext = '.exe'
+
+mf_exe_name = os.path.join(bin_path, "mfnwt")
+pp_exe_name = os.path.join(bin_path, "pestpp")
+ies_exe_name = os.path.join(bin_path, "pestpp-ies")
+swp_exe_name = os.path.join(bin_path, "pestpp-swp")
+
+
+def freyberg_test():
+    import shutil
+
+    import numpy as np
+    import pandas as pd
+    try:
+        import flopy
+    except:
+        return
+    import pyemu
+
+    ext = ''
+    bin_path = os.path.join("..", "..", "bin")
+    if "linux" in platform.platform().lower():
+        bin_path = os.path.join(bin_path, "linux")
+    elif "darwin" in platform.platform().lower():
+        bin_path = os.path.join(bin_path, "mac")
+    else:
+        bin_path = os.path.join(bin_path, "win")
+        ext = '.exe'
+
+    org_model_ws = os.path.join("..", "examples", "freyberg_sfr_update")
+    nam_file = "freyberg.nam"
+    m = flopy.modflow.Modflow.load(nam_file, model_ws=org_model_ws, check=False, forgive=False,
+                                   exe_name=mf_exe_name)
+    org_model_ws = "temp2"
+    m.external_path="."
+    m.change_model_ws(org_model_ws)
+    m.write_input()
+    print("{0} {1}".format(mf_exe_name, m.name + ".nam"), org_model_ws)
+    os_utils.run("{0} {1}".format(mf_exe_name, m.name + ".nam"), cwd=org_model_ws)
+
+    pf = PstFrom(original_d=org_model_ws,new_d="new_temp",remove_existing=True)
+    pf.add_uniform_pars(filenames="rech_1.ref")
+    pf.add_uniform_pars(filenames=["WEL_0000.dat"])
+
+
+if __name__ == "__main__":
+    freyberg_test()
--- pyemu/prototypes/pst_from.py
+++ pyemu/prototypes/pst_from.py
@@ -12,7 +12,12 @@ import pyemu
 
 class PstFrom(object):
 
-    def __init__(self,original_d,new_d,longnames=True):
+    def __init__(self,original_d,new_d,longnames=True,remove_existing=False):
+        self.original_d = original_d
+        self.new_d = new_d
+        self.original_file_d = None
+        self.mult_file_d = None
+        self.remove_existing = bool(remove_existing)
         self.par_dfs = []
         self.obs_dfs = []
         self.pre_py_cmds = []
@@ -22,8 +27,12 @@ class PstFrom(object):
         self.post_sys_cmds = []
 
         self.longnames=bool(longnames)
-        self.logger = pyemu.Logger("PstFrom.log")
-        #todo: setup dirs, add some intro log info
+        self.logger = pyemu.Logger("PstFrom.log",echo=True)
+
+        self.logger.statement("starting PstFrom process")
+
+        self._setup_dirs()
+
 
     def _add_pars(self,filenames,dist_type,sigma_range,
                         upper_bound,lower_bound):
@@ -34,15 +43,99 @@ class PstFrom(object):
 
 
     def _setup_dirs(self):
-        pass
+        self.logger.log("setting up dirs")
+        if not os.path.exists(self.original_d):
+            self.logger.lraise("original_d '{0}' not found".format(self.original_d))
+        if not os.path.isdir(self.original_d):
+            self.logger.lraise("original_d '{0}' is not a directory".format(self.original_d))
+        if os.path.exists(self.new_d):
+            if self.remove_existing:
+                self.logger.log("removing existing new_d '{0}'".format(self.new_d))
+                shutil.rmtree(self.new_d)
+                self.logger.log("removing existing new_d '{0}'".format(self.new_d))
+            else:
+                self.logger.lraise("new_d '{0}' already exists - use remove_existing=True".format(self.new_d))
 
-    def _par_filename_prep(self,filenames,index_cols,par_cols):
+        self.logger.log("copying original_d '{0}' to new_d '{1}'".format(self.original_d,self.new_d))
+        shutil.copytree(self.original_d,self.new_d)
+        self.logger.log("copying original_d '{0}' to new_d '{1}'".format(self.original_d, self.new_d))
+
+
+        self.original_file_d = os.path.join(self.new_d, "org")
+        if os.path.exists(self.original_file_d):
+            self.logger.lraise("'org' subdir already exists in new_d '{0}'".format(self.new_d))
+        os.makedirs(self.original_file_d)
+
+        self.mult_file_d = os.path.join(self.new_d, "mult")
+        if os.path.exists(self.mult_file_d):
+            self.logger.lraise("'mult' subdir already exists in new_d '{0}'".format(self.new_d))
+        os.makedirs(self.mult_file_d)
+
+        self.logger.log("setting up dirs")
+
+    def _par_filename_prep(self,filenames,index_cols,use_cols):
         pass
-        # todo: if par_cols, then index_cols
-        # todo: parse index_cols based on type (int or str)
-        # todo: if par_cols, then par_cols types must be same as index_cols types
+        # todo: cast str column names, index_cols and use_cols to lower if str?
         # todo: copy files, load files, return file_dict
-    
+        file_dict = {}
+        if not isinstance(filenames,list):
+            filenames = [filenames]
+        if index_cols is None and use_cols is not None:
+            self.logger.lraise("index_cols is None, but use_cols is not ({0})".format(str(par_cols)))
+        # load list type files
+        if index_cols is not None:
+            if not isinstance(index_cols,list):
+                index_cols = [index_cols]
+            if isinstance(index_cols[0],str):
+                header=0
+            elif isinstance(index_cols[0],int):
+                header=None
+            else:
+                self.logger.lraise("unrecognized type for index_cols, should be str or int, not {0}".
+                                   format(str(type(index_cols[0]))))
+            if use_cols is not None:
+                if not isinstance(use_cols,list):
+                    use_cols = [use_cols]
+                if isinstance(use_cols[0], str):
+                    header = 0
+                elif isinstance(use_cols[0], int):
+                    header = None
+                else:
+                    self.logger.lraise("unrecognized type for use_cols, should be str or int, not {0}".
+                                       format(str(type(use_cols[0]))))
+                itype = type(index_cols[0])
+                utype = type(use_cols[0])
+                if itype != utype:
+                    self.logger.lraise("index_cols type '{0} != use_cols type '{1}'".
+                                       format(str(itype),str(utype)))
+
+
+            delim_whitespace = True
+            if filename.lower().endswith(".csv"):
+                delim_whitespace=False
+
+            for filename in filenames:
+                file_path = os.path.join(self.new_d, filename)
+                self.logger.log("loading array {0}".format(file_path))
+                if not os.path.exists(file_path):
+                    self.logger.lraise("par filename '{0}' not found ".format(file_path))
+                df = pd.read_csv(file_path,header=header,delim_whitespace=delim_whitespace)
+
+                self.logger.log("loading array {0}".format(file_path))
+
+        # load array type files
+        else:
+            for filename in filenames:
+                file_path = os.path.join(self.new_d, filename)
+                self.logger.log("loading array {0}".format(file_path))
+                if not os.path.exists(file_path):
+                    self.logger.lraise("par filename '{0}' not found ".format(file_path))
+                arr = np.loadtxt(os.path.join(self.new_d,filename))
+                self.logger.log("loading array {0}".format(file_path))
+                self.logger.statement("loaded array '{0}' of shape {1}".format(filename,arr.shape))
+                np.savetxt(os.path.join(self.original_file_d,filename),arr)
+                file_dict[filename] = arr
+
 
     def add_pars_from_template(self,tpl_filename, in_filename):
         pass
@@ -50,14 +143,18 @@ class PstFrom(object):
 
     def add_uniform_pars(self,filenames,dist_type="gaussian",sigma_range=4.0,
                                upper_bound=1.0e10,lower_bound=1.0e-10,trans="log",
-                               prefix="uni",index_cols=None,par_cols=None):
-        pass
+                               prefix="uni",index_cols=None,use_cols=None):
+        self.logger.log("adding uniform pars for file(s) {0}".format(str(filenames)))
+        file_dict = self._par_filename_prep(filenames,index_cols,use_cols)
+
+
+        self.logger.log("adding uniform pars for file(s) {0}".format(str(filenames)))
 
 
     def add_zone_pars(self, filenames, zone_array, dist_type="gaussian",
                       sigma_range=4.0,upper_bound=1.0e10,
                       lower_bound=1.0e-10, trans="log",prefix="zon",
-                      skip_zone_vals=None,index_cols=None,par_cols=None):
+                      skip_zone_vals=None,index_cols=None,use_cols=None):
 
         #todo: check for zone array shape compatibility
         #todo: if list pars, what if zone array is not 3D?
@@ -68,7 +165,7 @@ class PstFrom(object):
                       sigma_range=4.0,upper_bound=1.0e10,
                       lower_bound=1.0e-10, trans="log",prefix="grd",
                       skip_zone_vals=None,geostruct=None,spatial_ref=None,
-                      index_cols=None,par_cols=None):
+                      index_cols=None,use_cols=None):
 
         # todo: geostruct and spatial_ref must be used together
         # todo: if list pars, what if zone array is not 3D?
@@ -79,7 +176,7 @@ class PstFrom(object):
                             sigma_range=4.0,upper_bound=1.0e10,
                             lower_bound=1.0e-10, trans="log",prefix="ppt",
                             space=10,geostruct=None,spatial_ref=None,
-                            index_cols=None,par_cols=None):
+                            index_cols=None,use_cols=None):
 
         # todo: geostruct and spatial_ref must be used together
         # todo: if list pars, what if zone array is not 3D?
@@ -90,7 +187,7 @@ class PstFrom(object):
                     sigma_range=4.0,upper_bound=1.0e10,
                     lower_bound=1.0e-10, trans="log",prefix="ppt",
                     num_comps=10,geostruct=None,spatial_ref=None,
-                    index_cols=None,par_cols=None):
+                    index_cols=None,use_cols=None):
 
         # todo: geostruct and spatial_ref must be used together
         # todo: num_comps <= array entries
