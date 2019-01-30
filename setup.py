47e89fb5257a0fa95054afb50eeee497356ee208
Jeremy White <jtwhite1000@gmail.com>
2019-01-30 14:23:30 +1300

    update to version 0.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup
 
 long_description = \
-"""pyemu is a set of python modules for linear-based model-independent uncertainty analyses.
+"""pyemu is a set of python modules for interfacing with PEST and PEST++.
 """ 
 
 setup(name="pyemu",
@@ -12,8 +12,8 @@ setup(name="pyemu",
       author="Jeremy White, Mike Fienen",
       author_email='jtwhite1000@gmail.com,mnfienen@usgs.gov',
       url='https://github.com/jtwhite79/pyemu',
-      download_url = 'https://github.com/jtwhite79/pyemu/tarball/0.6',
+      download_url = 'https://github.com/jtwhite79/pyemu/tarball/0.7',
       license='New BSD',
-      platforms='Windows, Mac OS-X',
+      platforms='Windows, Mac OS-X, linux',
       packages = ["pyemu","pyemu.pst","pyemu.plot","pyemu.utils","pyemu.mat","pyemu.prototypes"],
-      version="0.6")
+      version="0.7")
