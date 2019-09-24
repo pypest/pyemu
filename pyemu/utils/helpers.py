4b730e915b464a01a214922fa624140e1a142801
White <jtwhite1000@gmail.com>
2019-09-24 10:28:36 -0600

    working on support for multiprocessing on windows

--- pyemu/utils/helpers.py
+++ pyemu/utils/helpers.py
@@ -2463,23 +2463,25 @@ class PstFromFlopyModel(object):
             f.write("import os\nimport multiprocessing as mp\nimport numpy as np"+\
                     "\nimport pandas as pd\nimport flopy\n")
             f.write("import pyemu\n")
-            f.write("if __name__ == '__main__':\n")
-            f.write("    mp.freeze_support()\n")
+            f.write("def main():\n")
             f.write("\n")
+            s = "    "
             for ex_imp in self.extra_forward_imports:
-                f.write('import {0}\n'.format(ex_imp))
+                f.write(s+'import {0}\n'.format(ex_imp))
             for tmp_file in self.tmp_files:
-                f.write("try:\n")
-                f.write("   os.remove('{0}')\n".format(tmp_file))
-                f.write("except Exception as e:\n")
-                f.write("   print('error removing tmp file:{0}')\n".format(tmp_file))
+                f.write(s+"try:\n")
+                f.write(s+"   os.remove('{0}')\n".format(tmp_file))
+                f.write(s+"except Exception as e:\n")
+                f.write(s+"   print('error removing tmp file:{0}')\n".format(tmp_file))
             for line in self.frun_pre_lines:
-                f.write(line+'\n')
+                f.write(s+line+'\n')
             for line in self.frun_model_lines:
-                f.write(line+'\n')
+                f.write(s+line+'\n')
             for line in self.frun_post_lines:
-                f.write(line+'\n')
+                f.write(s+line+'\n')
             f.write("\n")
+            f.write("if __name__ == '__main__':\n")
+            f.write("    mp.freeze_support()\n    main()\n\n")
 
 
     def _parse_k(self, k, vals):
