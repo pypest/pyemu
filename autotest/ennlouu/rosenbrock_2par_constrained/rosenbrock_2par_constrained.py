d857a77556d1b5c46da707c5f134f3c5170d5bcb
Matt Knowling <mjknowling@gmail.com>
2020-01-06 18:07:11 +1300

    add rosen linear constraint case.. again

--- autotest/ennlouu/rosenbrock_2par_constrained/rosenbrock_2par_constrained.py
+++ autotest/ennlouu/rosenbrock_2par_constrained/rosenbrock_2par_constrained.py
@@ -7,7 +7,7 @@ result = 100.0*(x2 - x1**2.0)**2.0 + (1 - x1)**2.0
 # see: https://docs.scipy.org/doc/scipy-0.15.1/reference/generated/scipy.optimize.rosen.html
 
 #constraint = x1**2 + x2**2  # (==/<=/>= 1) eq or ineq constraint based on dec vars
-constraint = -6*x1 - x2
+constraint = 6*x1 + x2
 
 with open(os.path.join("obs.dat"),'w') as f:
 	f.write("{0:20.8E}\n".format(result))
