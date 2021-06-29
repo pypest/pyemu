b84a4ea8018b8cbb8e7aa6b237e601777e69857b
Jeremy White <jtwhite1000@gmail.com>
2021-06-29 09:59:19 -0600

    added some more to the pstfrom mf6 notebok

--- examples/helpers.py
+++ examples/helpers.py
@@ -1,7 +1,31 @@
+import os
 
 def process_model_outputs():
 	import numpy as np
 	print("processing model outputs")
 	arr = np.random.random(100)
-	np.savetxt("test.dat",arr)
+	np.savetxt("special_outputs.dat",arr)
+	return arr
+
+
+def write_ins_file(d):
+	cwd = os.getcwd()
+	os.chdir(d)
+	arr = process_model_outputs()
+	os.chdir(cwd)
+	with open(os.path.join(d,"special_outputs.dat.ins"),'w') as f:
+		f.write("pif ~\n")
+		for i in range(arr.shape[0]):
+			f.write("l1 !sobs_{0}!\n".format(i))
+
+	import pyemu
+	i = pyemu.pst_utils.InstructionFile(os.path.join(d,"special_outputs.dat.ins"))
+	df = i.read_output_file("special_outputs.dat")
+	
+	return df
+
+
+if __name__ == "__main__":
+	#process_model_outputs()
+	write_ins_file(".")
 	
