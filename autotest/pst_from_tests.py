82bfd430e2b753f747c12843bfac7511011abce9
Brioch <briochh@gmail.com>
2021-08-03 09:58:52 +1200

    converting obs and par names to lower.
    consolidating some of the methods across direct and mult application

--- autotest/pst_from_tests.py
+++ autotest/pst_from_tests.py
@@ -3512,7 +3512,7 @@ if __name__ == "__main__":
     mf6_freyberg_test()
     #mf6_freyberg_da_test()
     #mf6_freyberg_shortnames_test()
-    # mf6_freyberg_direct_test()
+    mf6_freyberg_direct_test()
     #mf6_freyberg_varying_idomain()
     #xsec_test()
     #mf6_freyberg_short_direct_test()
