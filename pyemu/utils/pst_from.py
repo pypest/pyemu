de8cd0ddf8433c368767509d842cbbe0a36cfdcb
jdub <jtwhite1000@gmail.com>
2021-10-07 13:06:50 -0600

    added a few more checks for agreement with bounds and initial value in pstfrom add_pars()

--- pyemu/utils/pst_from.py
+++ pyemu/utils/pst_from.py
@@ -1758,6 +1758,14 @@ class PstFrom(object):
         if lower_bound is None:
             lower_bound = -1.0e+10
 
+        if lower_bound >= upper_bound:
+            self.logger.lraise("lower_bound {0} >= upper_bound {1}".format(lower_bound,upper_bound))
+        if lower_bound > initial_value:
+            self.logger.lraise("lower_bound {0} > initial_value {1}".format(lower_bound,initial_value))
+        if upper_bound < initial_value:
+            self.logger.lraise("upper_bound {0} < initial_value {1}".format(upper_bound,initial_value))
+
+
         if isinstance(filenames, str) or isinstance(filenames, Path):
             filenames = [filenames]
         # data file paths relative to the pest parent directory
