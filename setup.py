a73547a5ad277da1ab93c67c352efd458714b530
jdub <jtwhite1000@gmail.com>
2021-11-16 16:28:34 -0700

    more pip bs

--- setup.py
+++ setup.py
@@ -6,14 +6,15 @@ long_description = \
 """pyemu is a set of python modules for interfacing with PEST and PEST++.
 """ 
 
-setup(name="pyemu",
-      description=long_description,
-      long_description=long_description,      
-      author="Jeremy White, Mike Fienen,Brioch Hemmings",
-      author_email='jtwhite1000@gmail.com,mnfienen@usgs.gov,b.hemmings@gns.cri.nz',
-      url='https://github.com/pypest/pyemu',
-      download_url = 'https://github.com/jtwhite79/pyemu/tarball/1.1.0',
-      license='New BSD',
-      platforms='Windows, Mac OS-X, Linux',
-      packages = ["pyemu","pyemu.pst","pyemu.plot","pyemu.utils","pyemu.mat","pyemu.prototypes"],
-      version="1.1.0")
+setup()
+# name="pyemu",
+#       description=long_description,
+#       long_description=long_description,      
+#       author="Jeremy White, Mike Fienen,Brioch Hemmings",
+#       author_email='jtwhite1000@gmail.com,mnfienen@usgs.gov,b.hemmings@gns.cri.nz',
+#       url='https://github.com/pypest/pyemu',
+#       download_url = 'https://github.com/jtwhite79/pyemu/tarball/1.1.0',
+#       license='New BSD',
+#       platforms='Windows, Mac OS-X, Linux',
+#       packages = ["pyemu","pyemu.pst","pyemu.plot","pyemu.utils","pyemu.mat","pyemu.prototypes"],
+#       version="1.1.0")
