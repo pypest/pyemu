2106553ca47ea9ac9e57193bcf3f5df34d4cef01
jdub <jtwhite1000@gmail.com>
2023-07-19 17:01:01 +0800

    more work on ac draw opt

--- autotest/utils_tests.py
+++ autotest/utils_tests.py
@@ -2310,12 +2310,23 @@ def ac_draw_test(tmp_path):
     obsval.append(2.0)
     ogrps.append("less_")
     ogrps.append("greater_")
+    onames.append("zero1")
+    obsval.append(1.0)
+    ogrps.append("zero")
+
+    onames.append("zero2")
+    obsval.append(1.0)
+    ogrps.append("less_zero")
+    onames.append("zero3")
+    obsval.append(1.0)
+    ogrps.append("greater_zero")
 
     pst = pyemu.Pst.from_par_obs_names(obs_names=onames)
     pst.observation_data.loc[onames,"obgnme"] = ogrps
-    pst.observation_data.loc[onames[:-2],"distance"] = distance
+    pst.observation_data.loc[onames[:-5],"distance"] = distance
     pst.observation_data.loc[onames,"obsval"] = obsval
     pst.observation_data.loc[onames, "weight"] = 0.000001#1/(np.array(obsval))
+    pst.observation_data.loc[["zero1","zero2","zero3"],"weight"] = 0.0
     print(obsval)
     pst.observation_data.loc[onames, "standard_deviation"] = np.array(obsval) * 0.1
     pst.observation_data.loc[onames, "lower_bound"] = np.array(obsval).min()
