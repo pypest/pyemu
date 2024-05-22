71c053046a21f70c438a0b028ca57214992e9635
nicohiggs <nicoreekohiggs@gmail.com>
2024-05-22 11:10:15 -0600

    change _enforce_reset() to use df.clip()

--- pyemu/en.py
+++ pyemu/en.py
@@ -1697,11 +1697,6 @@ class ParameterEnsemble(Ensemble):
         violating vals to bound
         """
 
-        ub = (self.ubnd * (1.0 - bound_tol)).to_dict()
-        lb = (self.lbnd * (1.0 + bound_tol)).to_dict()
-
-        val_arr = self._df.to_numpy()
-        for iname, name in enumerate(self.columns):
-            val_arr[val_arr[:, iname] > ub[name], iname] = ub[name]
-            val_arr[val_arr[:, iname] < lb[name], iname] = lb[name]
-        self._df = pd.DataFrame(val_arr, index=self._df.index, columns=self._df.columns)
+        ub = self.ubnd * (1.0 - bound_tol)
+        lb = self.lbnd * (1.0 + bound_tol)
+        self._df = self._df.clip(lb, ub, axis=1)
