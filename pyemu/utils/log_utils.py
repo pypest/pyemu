53d46fbad4236bc0d5221a85d8637b0cc4d1d1b9
Hugo Lapré <hugo.lapre@brabantwater.nl>
2024-10-13 20:24:17 +0200

    feat: add basicConfig parameter option

--- pyemu/utils/log_utils.py
+++ pyemu/utils/log_utils.py
@@ -2,7 +2,7 @@
 
 import logging
 import sys
-from typing import Optional
+from typing import Any, Optional
 
 FILE_HANDLER = logging.FileHandler("pyemu.log", delay=True)
 STREAM_HANDLER = logging.StreamHandler(sys.stdout)
@@ -10,13 +10,30 @@ STREAM_HANDLER = logging.StreamHandler(sys.stdout)
 LOGGER: Optional[logging.Logger] = None
 
 
-def set_logger(logger: logging.Logger) -> None:
+def set_logger(
+    logger: Optional[logging.Logger] = None,
+    /,
+    **basic_config: Any,
+) -> None:
     """Set the global logger to be used by pyemu.
 
     Args:
         logger (logging.Logger): the logger to be used.
+        **basic_config (Any): keyword arguments to `logging.basicConfig`
     """
     global LOGGER
+    if logger is not None:
+        if basic_config:
+            raise ValueError(
+                "If a logger is passed no extra keyword arguments should be passed as well."
+            )
+    else:
+        if not basic_config:
+            raise ValueError(
+                "If no logger is passed then keyword arguments for logging.basicConfig should be passed."
+            )
+        logging.basicConfig(**basic_config)
+        logger = logging.getLogger("pyemu")
     LOGGER = logger
 
 
