c34e784e129a9bee90a025176af443a0a2f48dc6
rhugman <rthugman@gmail.com>
2024-10-23 09:50:00 +0100

    switched illegal char catch to prep_pp_hyperpar() funx

--- pyemu/utils/pst_from.py
+++ pyemu/utils/pst_from.py
@@ -4042,7 +4042,11 @@ def prep_pp_hyperpars(file_tag,pp_filename,pp_info,out_filename,grid_dict,
     except Exception as e:
         raise Exception("prep_pp_hyperpars() error importing pypestutils: '{0}'".format(str(e)))
 
-
+    illegal_chars = [i for i in r"/:*?<>\|"]
+    for i in illegal_chars:
+        print("warning: replacing illegal character '{0}' with '-' in file_tag name '{1}'".format(i,pg))
+        file_tag = file_tag.replace(i,"-")
+        
     gridinfo_filename = file_tag + ".gridinfo.dat"
     corrlen_filename = file_tag + ".corrlen.dat"
     bearing_filename = file_tag + ".bearing.dat"
