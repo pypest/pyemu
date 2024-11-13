394c92f0801532c7f364798ed411c6871a9b6db9
Brioch <briochh@gmail.com>
2024-11-14 12:14:07 +1300

    ppu3 all tests, just py3.11

--- autotest/conftest.py
+++ autotest/conftest.py
@@ -5,18 +5,18 @@ import pytest
 pytest_plugins = ["modflow_devtools.fixtures"]
 
 collect_ignore = [
-    "utils_tests.py",
-    "pst_tests.py",
-    "pst_tests_2.py",
-    "pst_from_tests.py",
+    # "utils_tests.py",
+    # "pst_tests.py",
+    # "pst_tests_2.py",
+    # "pst_from_tests.py",
     # "en_tests.py",
-    "full_meal_deal_tests_2.py",
+    # "full_meal_deal_tests_2.py",
     # "la_tests.py",
-    "plot_tests.py",
-    "metrics_tests.py",
-    "moouu_tests.py",
-    "mat_tests.py",
-    "da_tests.py",
+    # "plot_tests.py",
+    # "metrics_tests.py",
+    # "moouu_tests.py",
+    # "mat_tests.py",
+    # "da_tests.py",
     # "get_pestpp_tests.py"
 ]
 
