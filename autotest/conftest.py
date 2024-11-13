fd0e174a399a0b9c35982005dcac2499a6f1424b
Brioch <briochh@gmail.com>
2024-11-14 11:50:15 +1300

    en and la test

--- autotest/conftest.py
+++ autotest/conftest.py
@@ -9,14 +9,15 @@ collect_ignore = [
     "pst_tests.py",
     "pst_tests_2.py",
     "pst_from_tests.py",
-    "en_tests.py",
+    # "en_tests.py",
     "full_meal_deal_tests_2.py",
     # "la_tests.py",
     "plot_tests.py",
     "metrics_tests.py",
     "moouu_tests.py",
     "mat_tests.py",
-    "da_tests.py"
+    "da_tests.py",
+    # "get_pestpp_tests.py"
 ]
 
 @pytest.fixture(autouse=True)
