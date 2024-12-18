278cc4ca0a4b7197d81b88c3775cb04b1f78fe01
jwhite <jtwhite1000@gmail.com>
2024-12-18 11:55:44 -0700

    initial prototype

---
+++ autotest/result_handler_prototype.py
@@ -0,0 +1,142 @@
+import os
+import pandas as pd
+import pyemu 
+# class ResultFile(object):
+# 	def __init__(self,resulttype):
+# 		if resulttype == "par_ensembles":
+# 			print(resulttype)
+# 		#print(stuff)
+
+
+class ResultHandler(object):
+	def __init__(self,m_d,case):
+		self.m_d = m_d
+		self.case = case
+		self.path2files = [os.path.join(self.m_d,f.lower()) for f in os.listdir(self.m_d)]
+		self.results_loaded = {}
+		#todo: check that m_ds are infact valid master dirs
+
+	def get_ensemble_files(self,tag):
+		files = []	
+		case_tag = self.case + tag
+
+		for f in self.path2files:
+			#print(tag,f)
+			if tag in f.lower() and\
+			    os.path.split(f)[1].lower().startswith(self.case+".") and \
+			    len(os.path.split(f)[1].split(".")) == 4:
+			    try:
+			        itr = int(os.path.split(f)[1].split(".")[1])
+			    except Exception as e:
+			        continue
+			    files.append(f)
+		return files
+
+
+class ResultIesHandler(ResultHandler):
+
+	def __getattr__(self,tag):
+		tag = tag.lower().strip()
+		if tag.startswith("par_en") or tag.startswith("obs_en"):
+			itr = self.get_ensemble_iter(tag)
+			ttag = tag.split("_")[0]
+			#load the combined ensemble
+			if itr is None:
+				file_tag = ".{0}.".format(ttag)
+				files = self.get_ensemble_files(file_tag)
+				print(files)
+				if len(files) == 0:
+					raise Exception()
+				itrs = [int(os.path.split(f)[1].split('.')[1]) for f in files]
+				d = {i:f for i,f in zip(itrs,files)}
+				itrs.sort()
+				dfs = []
+				for itr in itrs:
+					df = self.get_or_load_ensemble_file(d[itr])
+					dfs.append(df)
+				df = pd.concat(dfs,keys=itrs,names=["iteration","real_name"])
+				return df
+			else:
+				file_tag = ".{0}.{1}.".format(itr,ttag)
+				files = self.get_ensemble_files(file_tag)
+				if len(files) != 1:
+					#todo something here...
+					raise Exception()
+
+				df = self.get_or_load_ensemble_file(files[0])
+				return df
+				
+		
+		elif tag.startswith("phi"):
+			phi_type = tag.split("_")[1]
+			print(phi_type)
+			pass
+		
+		#todo: more file types here...
+
+		else:
+			raise Exception("tag: '{0}' not recognized".format(tag))
+
+	
+	def get_ensemble_iter(self,tag):
+		if "ensemble" in tag:
+			itr = tag.split("ensemble")[1]
+		elif "en" in tag:
+			itr = tag.split("en")[1]
+		else:
+			raise Exception()
+		if itr == "":
+			itr = None
+		return itr
+		
+	def get_or_load_ensemble_file(self,filename):
+		if filename in self.results_loaded:
+			return self.results_loaded[filename]
+		else:
+			if filename.lower().endswith(".csv"):
+				df = pd.read_csv(filename,index_col=0)
+			elif filename.lower().endswith(".jcb"):
+				df = pyemu.Matrix.from_binary(filename).to_dataframe()
+			self.results_loaded[filename] = df
+
+			return df
+
+
+	def load_csv(self,tags):
+		pass
+
+
+class ResultMouHandler(ResultHandler):
+	def __getattr__(self,tag):
+		print(tag)
+		#return ResultFile(tag)
+		pass
+
+
+class Results(object):
+	def __init__(self,m_d,case=None):
+		#todo: if case is none, look for one and only one control file in m_d
+		
+		self.m_d = m_d
+		self.case = case
+		self.ieshand = ResultIesHandler(self.m_d,self.case)
+		self.mouhand = ResultMouHandler(self.m_d,self.case)
+		
+
+	def __getattr__(self,tag):
+		
+		if tag == "ies":
+			return self.ieshand
+		elif tag == "mou":
+			return self.mouhand
+		else:
+			raise Exception()
+
+
+r = Results(m_d=os.path.join("pst","master_ies1"),case="pest")
+#get the prior par en
+print(r.ies.par_en0)
+# get the 1st iter obs en
+print(r.ies.obs_ensemble1)
+#get the combined par en across all iters
+print(r.ies.par_en)
