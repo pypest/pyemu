69ba5366b3d828051c08a6c3f28a06762b85268e
jwhite <jtwhite1000@gmail.com>
2024-12-23 16:06:27 -0700

    more

--- autotest/result_handler_prototype.py
+++ autotest/result_handler_prototype.py
@@ -34,7 +34,7 @@ class ResultHandler(object):
                 self.failed2load_files.append([filename, str(e)])
             return df
 
-    def get_or_load_ensemble_file(self, filename):
+    def get_or_load_ensemble_file(self, filename,index_name="realization"):
         if len(os.path.split(filename)) == 1:
             filename = os.path.join(self.m_d,filename)
         if filename in self.results_loaded:
@@ -46,13 +46,23 @@ class ResultHandler(object):
                     df = pd.read_csv(filename, index_col=0)
                 elif filename.lower().endswith(".jcb") or filename.lower().endswith(".jco") or filename.lower().endswith(".bin"):
                     df = pyemu.Matrix.from_binary(filename).to_dataframe()
+                df.index.name = index_name
                 self.results_loaded[filename] = df
             except Exception as e:
                 print("error loading file '{0}': {1}".format(filename,str(e)))
                 self.failed2load_files.append([filename,str(e)])
             return df
 
-
+    def check_dup_iters(self,itrs, files):
+        dup_itrs,dup_files = [],[]
+        for i,itr in enumerate(itrs[:-1]):
+            if itr in itrs[i+1:]:
+                dup_itrs.append(str(itr))
+                dup_files.append(files[i])
+        if len(dup_files) > 0:
+            print("dup files: {0}".format("\n".join(dup_files)))
+            print("dup itrs: {0}".format("\n".join(dup_itrs)))
+            raise Exception("duplicate iteration tags found")
 
 
 
@@ -103,13 +113,19 @@ class ResultIesHandler(ResultHandler):
                 if len(files) == 0:
                     raise Exception()
                 itrs = [int(os.path.split(f)[1].split('.')[1]) for f in files]
+                self.check_dup_iters(itrs,files)
                 d = {i:f for i,f in zip(itrs,files)}
                 itrs.sort()
                 dfs = []
                 for itr in itrs:
                     df = self.get_or_load_ensemble_file(d[itr])
                     dfs.append(df)
-                df = pd.concat(dfs,keys=itrs,names=["iteration","real_name"])
+                if len(dfs) == 0:
+                    return None
+                if len(dfs) > 1:
+                    df = pd.concat(dfs,keys=itrs,names=["iteration","realization"])
+                else:
+                    df = dfs[0]
                 return df
             else:
                 file_tag = ".{0}.{1}.".format(itr,ttag)
@@ -152,13 +168,19 @@ class ResultIesHandler(ResultHandler):
                 if len(files) == 0:
                     raise Exception()
                 itrs = [int(os.path.split(f)[1].split('.')[1]) for f in files]
+                self.check_dup_iters(itrs, files)
                 d = {i: f for i, f in zip(itrs, files)}
                 itrs.sort()
                 dfs = []
                 for itr in itrs:
                     df = self.get_or_load_csv_file(d[itr],index_col=0)
                     dfs.append(df)
-                df = pd.concat(dfs, keys=itrs, names=["iteration", "name"])
+                if len(dfs) == 0:
+                    return None
+                if len(dfs) > 1:
+                    df = pd.concat(dfs, keys=itrs, names=["iteration", "name"])
+                else:
+                    df = dfs[0]
                 return df
             else:
                 file_tag = ".{0}.{1}.".format(itr, ttag)
@@ -180,13 +202,19 @@ class ResultIesHandler(ResultHandler):
                 if len(files) == 0:
                     raise Exception()
                 itrs = [int(os.path.split(f)[1].split('.')[1]) for f in files]
+                self.check_dup_iters(itrs, files)
                 d = {i: f for i, f in zip(itrs, files)}
                 itrs.sort()
                 dfs = []
                 for itr in itrs:
                     df = self.get_or_load_csv_file(d[itr],index_col=0)
                     dfs.append(df)
-                df = pd.concat(dfs, keys=itrs, names=["iteration", "group"])
+                if len(dfs) == 0:
+                    return None
+                if len(dfs) > 1:
+                    df = pd.concat(dfs, keys=itrs, names=["iteration", "group"])
+                else:
+                    df = dfs[0]
                 return df
             else:
                 file_tag = ".{0}.{1}.".format(itr, ttag)
@@ -213,6 +241,12 @@ class ResultMouHandler(ResultHandler):
         case_tag = self.case + tag
 
         for f in self.path2files:
+            if "stack" in tag and "nested" not in tag and "nested" in f:
+                continue
+            if "pop" in tag and "archive" not in tag and "archive" in f:
+                continue
+            if "pop" in tag and "chance" not in tag and "chance" in f:
+                continue
             if tag in f.lower() and\
                 os.path.split(f)[1].lower().startswith(self.case+"."):
                 try:
@@ -228,10 +262,12 @@ class ResultMouHandler(ResultHandler):
             itr = tag.split("dvpop")[1]
         elif "obspop" in tag:
             itr = tag.split("obspop")[1]
-        elif tag == "stack_summary":
+        elif "stack_summary" in tag:
             itr = tag.split("stack_summary")[1]
         elif "archive" in tag:
             itr = tag.split("archive")[1]
+        elif "stack" in tag:
+            itr = tag.split("stack")[1]
         else:
             raise Exception("parse_iter_from_tag: unrecognized tag: '{0}'".format(tag))
         if itr == "":
@@ -260,13 +296,19 @@ class ResultMouHandler(ResultHandler):
                 if len(files) == 0:
                     raise Exception("no files found for tag '{0}'".format(file_tag))
                 itrs = [int(os.path.split(f)[1].split('.')[1]) for f in files]
+                self.check_dup_iters(itrs, files)
                 d = {i: f for i, f in zip(itrs, files)}
                 itrs.sort()
                 dfs = []
                 for itr in itrs:
-                    df = self.get_or_load_ensemble_file(d[itr])
+                    df = self.get_or_load_ensemble_file(d[itr],index_name="member")
                     dfs.append(df)
-                df = pd.concat(dfs, keys=itrs, names=["iteration", "real_name"])
+                if len(dfs) == 0:
+                    return None
+                if len(dfs) > 1:
+                    df = pd.concat(dfs, keys=itrs, names=["generation", "member"])
+                else:
+                    df = dfs[0]
                 return df
             else:
                 file_tag = ".{0}.{1}.".format(itr, ttag)
@@ -276,7 +318,7 @@ class ResultMouHandler(ResultHandler):
                     print(files)
                     raise Exception()
 
-                df = self.get_or_load_ensemble_file(files[0])
+                df = self.get_or_load_ensemble_file(files[0],index_name="member")
                 return df
 
 
@@ -288,6 +330,59 @@ class ResultMouHandler(ResultHandler):
             df = self.get_or_load_csv_file(csv_filename)
             return df
 
+
+        elif tag.startswith("parstack") or tag.startswith("obsstack") or \
+                tag.startswith("nestedparstack") or tag.startswith("nestedobstack"):
+            itr = self.parse_iter_from_tag(tag)
+            ttag = "par_stack"
+            if "obs" in tag:
+                ttag = "obs_stack"
+            if "nested" in tag:
+                ttag = "nested." + ttag
+            # load the combined ensemble
+            if itr is None:
+                file_tag = ".{0}.".format(ttag)
+                files = self.get_files(file_tag)
+                if len(files) == 0:
+                    raise Exception()
+                itrs = [int(os.path.split(f)[1].split('.')[1]) for f in files]
+                self.check_dup_iters(itrs,files)
+                d = {i: f for i, f in zip(itrs, files)}
+                itrs.sort()
+                dfs = []
+                for itr in itrs:
+                    df = self.get_or_load_ensemble_file(d[itr])
+                    if "nested" in tag:
+                        df["realization"] = df.index.map(lambda x: x.split("||")[0])
+                        df.index = df.index.map(lambda x: x.split("||")[1])
+
+                    dfs.append(df)
+                if len(dfs) == 0:
+                    return None
+                if "nested" in tag:
+                    df = pd.concat(dfs, keys=itrs, names=["generation", "member"])
+                else:
+                    if len(dfs) > 1:
+                        df = pd.concat(dfs, keys=itrs, names=["generation", "member"])
+                    else:
+                        df = dfs[0]
+
+                return df
+            else:
+                file_tag = ".{0}.{1}.".format(itr, ttag)
+                files = self.get_files(file_tag)
+                if len(files) != 1:
+                    # todo something here...
+                    raise Exception()
+
+                df = self.get_or_load_ensemble_file(files[0])
+                if "nested" in tag:
+                    df["realization"] = df.index.map(lambda x: x.split("||")[0])
+                    df.index = df.index.map(lambda x: x.split("||")[1])
+                    df.index.name = "member"
+                return df
+
+
         elif tag.startswith("stack_summary"):
             itr = self.parse_iter_from_tag(tag)
             ttag = "population_stack_summary"
@@ -298,13 +393,19 @@ class ResultMouHandler(ResultHandler):
                 if len(files) == 0:
                     raise Exception()
                 itrs = [int(os.path.split(f)[1].split('.')[1]) for f in files]
+                self.check_dup_iters(itrs, files)
                 d = {i: f for i, f in zip(itrs, files)}
                 itrs.sort()
                 dfs = []
                 for itr in itrs:
                     df = self.get_or_load_csv_file(d[itr], index_col=0)
                     dfs.append(df)
-                df = pd.concat(dfs, keys=itrs, names=["generation", "member"])
+                if len(dfs) == 0:
+                    return None
+                if len(dfs) > 1:
+                    df = pd.concat(dfs, keys=itrs, names=["generation", "member"])
+                else:
+                    df = dfs[0]
                 return df
             else:
                 file_tag = ".{0}.{1}.".format(itr, ttag)
@@ -348,6 +449,8 @@ class Results(object):
         else:
             raise Exception()
 
+
+
 def results_ies_1_test():
     r = Results(m_d=os.path.join("pst", "master_ies1"))
     # get all change sum files in an multiindex df
@@ -360,6 +463,7 @@ def results_ies_1_test():
 
     # weights
     df = r.ies.weight_en
+    #print(df)
     assert df is not None
 
     # various phi dfs
@@ -369,6 +473,7 @@ def results_ies_1_test():
     assert df is not None
     df = r.ies.phi_actual
     assert df is not None
+    print(df)
     df = r.ies.phi_meas
     assert df is not None
     # noise
@@ -383,6 +488,7 @@ def results_ies_1_test():
     # get the combined par en across all iters
     df = r.ies.par_en
     assert df is not None
+    #print(df)
 
 
 def results_ies_2_test():
@@ -423,61 +529,71 @@ def results_ies_2_test():
         assert df is not None
 
 def results_mou_1_test():
-    r = Results(m_d=os.path.join("pst", "zdt1_ascii"))
+    for m_d in [os.path.join("pst", "zdt1_bin"),os.path.join("pst", "zdt1_ascii")]:
+        r = Results(m_d=m_d)
 
-    df = r.mou.stack_summary
+        df = r.mou.nestedparstack
+        #print(df)
 
+        assert df is not None
 
-    df = r.mou.chanceobspop1
-    print(df)
-    assert df is not None
+        df = r.mou.parstack0
+        #print(df)
+        assert df is not None
 
-    df = r.mou.chanceobspop
-    print(df)
-    assert df is not None
-
-    df = r.mou.chancedvpop1
-    print(df)
-    assert df is not None
-
-    df = r.mou.chancedvpop
-    #print(df)
-    assert df is not None
-
-    df = r.mou.dvpop
-    assert df is not None
-
-    df = r.mou.dvpop0
-    #print(df)
-    assert df is not None
-
-    df = r.mou.obspop
-    #print(df)
-    assert df is not None
-
-    df = r.mou.obspop5
-    # print(df)
-    assert df is not None
-
-    df = r.mou.paretosum_archive
-    #print(df)
-    assert df is not None
-
-    df = r.mou.paretosum
-    #print(df)
-    assert df is not None
-
-    df = r.mou.archivedvpop
-    #print(df)
-    assert df is not None
-
-    df = r.mou.archiveobspop
-    #print(df)
-    assert df is not None
+        df = r.mou.stack_summary0
+        #print(df)
+        assert df is not None
 
 
+        df = r.mou.chanceobspop1
+        #print(df)
+        assert df is not None
+
+        df = r.mou.chanceobspop
+        #print(df)
+        assert df is not None
+
+        df = r.mou.chancedvpop1
+        #print(df)
+        assert df is not None
+
+        df = r.mou.chancedvpop
+        #print(df)
+        assert df is not None
+
+        df = r.mou.dvpop
+        assert df is not None
+
+        df = r.mou.dvpop0
+        #print(df)
+        assert df is not None
+
+        df = r.mou.obspop
+        #print(df)
+        assert df is not None
+
+        df = r.mou.obspop5
+        # print(df)
+        assert df is not None
+
+        df = r.mou.paretosum_archive
+        #print(df)
+        assert df is not None
+
+        df = r.mou.paretosum
+        #print(df)
+        assert df is not None
+
+        df = r.mou.archivedvpop
+        print(df)
+        assert df is not None
+
+        df = r.mou.archiveobspop
+        #print(df)
+        assert df is not None
 
 if __name__ == "__main__":
-    #results_ies_1_test()
-    #results_ies_2_test()
+    results_ies_1_test()
+    results_ies_2_test()
     results_mou_1_test()
