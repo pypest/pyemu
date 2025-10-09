1e0d6fd3920d8691ba6d36900d08c5e1e8b8cb4f
Brioch <briochh@gmail.com>
2025-10-09 07:54:17 +0100

    reducing test suite further for ci testing

--- autotest/conftest.py
+++ autotest/conftest.py
@@ -5,21 +5,21 @@ import pytest
 pytest_plugins = ["modflow_devtools.fixtures"]
 
 collect_ignore = [
-    # "emulator_tests.py",
-    # "en_tests.py",
-    # "full_meal_deal_tests_2.py",
-    # "get_pestpp_tests.py",
-    # "la_tests.py",
-    # "mat_tests.py",
-    # "mc_tests_ignore.py",
-    # "metrics_tests.py",
-    # "plot_tests.py",
-    # "pst_from_tests.py",
-    # "pst_tests.py",
-    # "pst_tests_2.py",
-    # "transformer_tests.py",
-    "utils_tests.py"
-    # "verf_test.py",
+    "emulator_tests.py",
+    "en_tests.py",
+    "full_meal_deal_tests_2.py",
+    "get_pestpp_tests.py",
+    "la_tests.py",
+    "mat_tests.py",
+    "mc_tests_ignore.py",
+    "metrics_tests.py",
+    "plot_tests.py",
+    "pst_from_tests.py",
+    "pst_tests.py",
+    "pst_tests_2.py",
+    "transformer_tests.py",
+    # "utils_tests.py"
+    "verf_test.py",
 ]
 
 @pytest.fixture(autouse=True)
