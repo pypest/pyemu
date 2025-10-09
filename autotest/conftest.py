76b8e53f17d975b1492dfe3f20f6a3669f58c754
Brioch <briochh@gmail.com>
2025-10-09 07:33:36 +0100

    trimming ci for testing

--- autotest/conftest.py
+++ autotest/conftest.py
@@ -6,18 +6,18 @@ pytest_plugins = ["modflow_devtools.fixtures"]
 
 collect_ignore = [
     # "utils_tests.py",
-    # "pst_tests.py",
-    # "pst_tests_2.py",
-    # "pst_from_tests.py",
-    # "en_tests.py",
-    # "full_meal_deal_tests_2.py",
-    # "la_tests.py",
-    # "plot_tests.py",
-    # "metrics_tests.py",
-    # "moouu_tests.py",
-    # "mat_tests.py",
-    # "da_tests.py",
-    # "get_pestpp_tests.py"
+    "pst_tests.py",
+    "pst_tests_2.py",
+    "pst_from_tests.py",
+    "en_tests.py",
+    "full_meal_deal_tests_2.py",
+    "la_tests.py",
+    "plot_tests.py",
+    "metrics_tests.py",
+    "moouu_tests.py",
+    "mat_tests.py",
+    "da_tests.py",
+    "get_pestpp_tests.py"
 ]
 
 @pytest.fixture(autouse=True)
